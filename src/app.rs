efcded11f332375d401ab05772f47e06dee212ab
nanachi <102416174+Swarkin@users.noreply.github.com>
2024-09-01 16:25:31 +0200

    absolute minimum template

--- src/app.rs
+++ src/app.rs
@@ -1,53 +1,16 @@
 use eframe::egui;
 
-pub struct TemplateApp {
-	label: String,
-	value: f32,
-}
-
-impl Default for TemplateApp {
-	fn default() -> Self {
-		Self {
-			label: "Hello World!".to_owned(),
-			value: 2.7,
-		}
-	}
-}
+#[derive(Default)]
+pub struct TemplateApp {}
 
 impl TemplateApp {
-	pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+	pub fn new(_cc: &eframe::CreationContext) -> Self {
 		Default::default()
 	}
 }
 
 impl eframe::App for TemplateApp {
-	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-		egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-			egui::menu::bar(ui, |ui| {
-				ui.menu_button("File", |ui| {
-					if ui.button("Quit").clicked() {
-						ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-					}
-				});
+	fn update(&mut self, _ctx: &egui::Context, _frame: &mut eframe::Frame) {
 
-				ui.with_layout(egui::Layout::right_to_left(egui::Align::Max), |ui| {
-					egui::widgets::global_dark_light_mode_buttons(ui);
-				});
-			});
-
-		});
-
-		egui::CentralPanel::default().show(ctx, |ui| {
-			ui.horizontal(|ui| {
-				ui.label("Write something: ");
-				ui.text_edit_singleline(&mut self.label);
-			});
-
-			ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-			if ui.button("Increment").clicked() {
-				self.value += 1.0;
-			}
-
-		});
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 mod app;
-use eframe::egui;
 
 fn main() -> eframe::Result {
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
-        ..Default::default()
-    };
     eframe::run_native(
         "eframe template",
-        native_options,
+        eframe::NativeOptions::default(),
         Box::new(|cc| Ok(Box::new(app::TemplateApp::new(cc)))),
     )
 }
