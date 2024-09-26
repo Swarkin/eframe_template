616d774fa7657ff2ab22749d15da7852ff2c2d4f
swarkin <102416174+Swarkin@users.noreply.github.com>
2024-09-26 13:56:12 +0200

    minimal web template

--- src/app.rs
+++ src/app.rs
@@ -15,7 +15,7 @@ impl Default for TemplateApp {
 }
 
 impl TemplateApp {
-	pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+	pub fn new(_cc: &eframe::CreationContext) -> Self {
 		Default::default()
 	}
 }
@@ -24,20 +24,22 @@ impl eframe::App for TemplateApp {
 	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
 		egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
 			egui::menu::bar(ui, |ui| {
-				ui.menu_button("File", |ui| {
-					if ui.button("Quit").clicked() {
-						ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-					}
-				});
+				if !cfg!(target_arch = "wasm32") {
+					ui.menu_button("File", |ui| {
+						if ui.button("Quit").clicked() {
+							ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+						}
+					});
+					ui.add_space(16.0);
+				}
 
-				ui.with_layout(egui::Layout::right_to_left(egui::Align::Max), |ui| {
-					egui::widgets::global_dark_light_mode_buttons(ui);
-				});
+				egui::widgets::global_dark_light_mode_buttons(ui);
 			});
-
 		});
 
 		egui::CentralPanel::default().show(ctx, |ui| {
+			ui.heading("eframe template");
+
 			ui.horizontal(|ui| {
 				ui.label("Write something: ");
 				ui.text_edit_singleline(&mut self.label);
@@ -48,6 +50,31 @@ impl eframe::App for TemplateApp {
 				self.value += 1.0;
 			}
 
+			ui.separator();
+
+			ui.add(egui::github_link_file!(
+                "https://github.com/emilk/eframe_template/blob/main/",
+                "Source code."
+            ));
+
+			ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+				powered_by_egui_and_eframe(ui);
+				egui::warn_if_debug_build(ui);
+			});
 		});
 	}
+}
+
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+	ui.horizontal(|ui| {
+		ui.spacing_mut().item_spacing.x = 0.0;
+		ui.label("Powered by ");
+		ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+		ui.label(" and ");
+		ui.hyperlink_to(
+			"eframe",
+			"https://github.com/emilk/egui/tree/master/crates/eframe",
+		);
+		ui.label(".");
+	});
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,52 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod app;
-use eframe::egui;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "eframe template",
-        native_options,
-        Box::new(|cc| Ok(Box::new(app::TemplateApp::new(cc)))),
-    )
+	let native_options = eframe::NativeOptions {
+		viewport: eframe::egui::ViewportBuilder::default()
+			.with_inner_size([400.0, 300.0])
+			.with_min_inner_size([300.0, 220.0]),
+		..Default::default()
+	};
+	eframe::run_native(
+		"eframe template",
+		native_options,
+		Box::new(|cc| Ok(Box::new(app::TemplateApp::new(cc)))),
+	)
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+	eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+	let web_options = eframe::WebOptions::default();
+
+	wasm_bindgen_futures::spawn_local(async {
+		let start_result = eframe::WebRunner::new()
+			.start(
+				"the_canvas_id",
+				web_options,
+				Box::new(|cc| Ok(Box::new(app::TemplateApp::new(cc)))),
+			)
+			.await;
+
+		let loading_text = web_sys::window()
+			.and_then(|w| w.document())
+			.and_then(|d| d.get_element_by_id("loading_text"));
+		if let Some(loading_text) = loading_text {
+			match start_result {
+				Ok(_) => {
+					loading_text.remove();
+				}
+				Err(e) => {
+					loading_text.set_inner_html(
+						"<p> The app has crashed. See the developer console for details. </p>",
+					);
+					panic!("Failed to start eframe: {e:?}");
+				}
+			}
+		}
+	});
 }
